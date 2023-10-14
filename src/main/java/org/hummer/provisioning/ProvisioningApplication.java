f327ab2bbbd3525e86c2bcab247fa8fb8a2a4948
dinge <dinge@DESKTOP-IDAGOMV.fritz.box>
2023-10-14 19:11:56 +0200

    test

--- src/main/java/org/hummer/provisioning/ProvisioningApplication.java
+++ src/main/java/org/hummer/provisioning/ProvisioningApplication.java
@@ -8,6 +8,7 @@ public class ProvisioningApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProvisioningApplication.class, args);
+		System.out.println("Test");
 	}
 
 }
