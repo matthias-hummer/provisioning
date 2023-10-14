63716e7d1b127a882d4eb1e9dc55d6b05da16ade
unknown <matthias-hummer>
2023-10-14 19:04:58 +0200

    second commit

---
+++ src/main/java/org/hummer/provisioning/ProvisioningApplication.java
@@ -0,0 +1,13 @@
+package org.hummer.provisioning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvisioningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvisioningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hummer/provisioning/ProvisioningApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hummer.provisioning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvisioningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
